8c549c7a62996c006bf09dadee4a86e2ff78c485
moti3027 <53740373+moti3027@users.noreply.github.com>
2026-08-20 14:52:24 +0300

    Update background.js
    Enhances the original No Bookmarklets extension by adding automatic removal of existing JavaScript bookmarklets.
    The extension scans the entire bookmark tree on installation, startup, and whenever bookmarks are created or changed, deleting any bookmarklet immediately.
    
    Signed-off-by: moti3027 <53740373+moti3027@users.noreply.github.com>

--- js/background.js
+++ js/background.js
@@ -4,9 +4,29 @@ async function scanBookmarkTree(bookmark) {
   if (!bookmark.id) return;
   if (!bookmark.url) return;
   if (!bookmark.url.toLowerCase().startsWith("javascript:")) return;
+
+  console.log("Deleting bookmarklet:", bookmark.title, bookmark.url);
   await chrome.bookmarks.remove(bookmark.id);
 }
-
-chrome.runtime.onInstalled.addListener(async () => scanBookmarkTree(await chrome.bookmarks.getTree()));
-chrome.bookmarks.onCreated.addListener(async id => scanBookmarkTree((await chrome.bookmarks.get([id]))[0]));
-chrome.bookmarks.onChanged.addListener(async id => scanBookmarkTree((await chrome.bookmarks.get([id]))[0]));
+chrome.runtime.onInstalled.addListener(async () => {
+  const tree = await chrome.bookmarks.getTree();
+  tree.forEach(scanBookmarkTree);
+});
+chrome.runtime.onStartup.addListener(async () => {
+  const tree = await chrome.bookmarks.getTree();
+  tree.forEach(scanBookmarkTree);
+});
+chrome.bookmarks.onCreated.addListener(async id => {
+  const bm = (await chrome.bookmarks.get([id]))[0];
+  scanBookmarkTree(bm);
+});
+chrome.bookmarks.onChanged.addListener(async id => {
+  const bm = (await chrome.bookmarks.get([id]))[0];
+  scanBookmarkTree(bm);
+});
+chrome.management.onEnabled.addListener(async info => {
+  if (info.id === chrome.runtime.id) {
+    const tree = await chrome.bookmarks.getTree();
+    tree.forEach(scanBookmarkTree);
+  }
+});
